0d8062ed0c79b2a66fbe00962a11e2766f91698c
toelapiut <toelapiut7@gmail.com>
2017-09-15 12:03:43 +0300

    added exports that contain fooddetails

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
+import { FoodDetails} from './food-details.model';
 
 @Component({
   selector: 'my-app',
   template: `
-    <div *ngIf="thing">
+    <div>
       <h1>Thing is true and therefore it is displayed!</h1>
     </div>
-  `
+`
 })
 
 export class AppComponent {
-  thing = "something that is not false";
+  public masterFoodList: FoodDetails[] = [
+    new FoodDetails("Hamburger","Didn't get a soda or cheese on my burger!",354)
+  ];
+
+  selectedFood: FoodDetails = null;
+  showFoodDetails(clickedFood: FoodDetails) {
+    this.selectedFood = clickedFood
+  }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { FoodDetails} from './food-details.model';
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    FoodDetails,
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
